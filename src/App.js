277e10981e4fda92dd184a420aeca32be6aaec86
tsj0124 <tsj6181@hotmail.com>
2022-05-08 16:38:54 +0800

    TodoApp upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		todos: [],
+	};
+
+	deleteTodo = (id) => {
+		const todos = this.state.todos.filter((todo) => {
+			return todo.id !== id;
+		});
+		this.setState({
+			todos,
+		});
+	};
+
+	addTodo = (todo) => {
+		todo.id = Math.random();
+		let todos = [...this.state.todos, todo];
+		this.setState({
+			todos,
+		});
+	};
+	render() {
+		return (
+			<div className="todo-app container">
+				<h1 className="center blue-text">Todo's</h1>
+				<Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+				<AddTodo addTodo={this.addTodo} />
+			</div>
+		);
+	}
 }
 
 export default App;
